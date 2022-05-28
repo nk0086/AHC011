78020f6019219945d82e2973f0cfd5b2ee705158
nk86 <n172586k@gmail.com>
2022-05-28 12:07:04 +0900

    settings

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Bytes, Chars},
+};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize, t: usize,
+        t: [[u64; n]; n]
+    }
+}
+
+fn solve() {
+    todo!()
+}
